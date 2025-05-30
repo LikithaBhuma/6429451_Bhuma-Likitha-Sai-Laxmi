0903ffa91caf1f60a35ef0f335b699445b541482
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 13:15:16 +0530

    Update ThreadExample.java

--- Module3/ThreadExample.java
+++ Module3/ThreadExample.java
@@ -1,29 +1,25 @@
 package Projects;
 class MyThread extends Thread {
     private String message;
-
     public MyThread(String message) {
         this.message = message;
     }
-
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
             System.out.println(Thread.currentThread().getName() + ": " + message);
             try {
-                Thread.sleep(500); // Simulate delay
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 System.out.println("Thread interrupted!");
             }
         }
     }
 }
-
 public class ThreadExample {
     public static void main(String[] args) {
         MyThread thread1 = new MyThread("Hello from Thread 1!");
         MyThread thread2 = new MyThread("Hello from Thread 2!");
-
         thread1.start();
         thread2.start();
     }
