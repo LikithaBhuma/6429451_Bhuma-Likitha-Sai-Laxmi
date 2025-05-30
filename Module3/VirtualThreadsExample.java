1b1423e075f03c9ca209598412c4ecf2ae671387
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 13:21:45 +0530

    Update VirtualThreadsExample.java

--- Module3/VirtualThreadsExample.java
+++ Module3/VirtualThreadsExample.java
@@ -1,17 +1,14 @@
 package Projects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 public class VirtualThreadsExample {
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-
         try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
             for (int i = 0; i < 100000; i++) {
                 executor.submit(() -> System.out.println("Hello from virtual thread: " + Thread.currentThread().getName()));
             }
         }
-
         long endTime = System.nanoTime();
         System.out.println("Execution Time (Virtual Threads): " + (endTime - startTime) / 1_000_000 + " ms");
     }
