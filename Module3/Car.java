28d7a6d90894856d8e4205f6371b7fc56d0bf42a
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:28:10 +0530

    Update Car.java

--- Module3/Car.java
+++ Module3/Car.java
@@ -1,18 +1,15 @@
 package Projects;
-//Define the Car class
 public class Car {
  // Attributes
 	private String make;
 	private String model;
 	private int year;
-
  // Constructor
 	public Car(String make, String model, int year) {
 		this.make = make;
 		this.model = model;
 		this.year = year;
 	}
-
  // Method to display car details
 	public void displayDetails() {
 		System.out.println("Car Details:");
@@ -20,12 +17,10 @@ public class Car {
 		System.out.println("Model: " + model);
 		System.out.println("Year: " + year);
 	}
-
 	public static void main(String[] args) {
      // Creating objects of Car class
 		Car car1 = new Car("Toyota", "Camry", 2022);
 		Car car2 = new Car("Honda", "Civic", 2021);
-
      // Calling method to display details
 		car1.displayDetails();
 		car2.displayDetails();
