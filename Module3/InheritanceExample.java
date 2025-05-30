26331369f340413050e82c58d75af38ed34ce2b5
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:44:48 +0530

    Update InheritanceExample.java

--- Module3/InheritanceExample.java
+++ Module3/InheritanceExample.java
@@ -12,9 +12,9 @@ class Dog extends Animal {
 }
 public class InheritanceExample {
 	public static void main(String[] args) {
-		Animal genericAnimal = new Animal();
-		Dog myDog = new Dog();
-		genericAnimal.makeSound();  
-		myDog.makeSound();          
+		Animal a = new Animal();
+		Dog d = new Dog();
+		a.makeSound();  
+		d.makeSound();          
 	}
 }
