a0e169668393aed7cc3aa93609551d348ad77402
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:32:46 +0530

    Update FileReadingExample.java

--- Module3/FileReadingExample.java
+++ Module3/FileReadingExample.java
@@ -2,20 +2,16 @@ package Projects;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 public class FileReadingExample {
     public static void main(String[] args) {
         try {
             File file = new File("output.txt");
-            Scanner scanner = new Scanner(file);
-
+            Scanner sc = new Scanner(file);
             System.out.println("Contents of output.txt:");
-
-            while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
             }
-
-            scanner.close();
+            sc.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error: File not found!");
         }
