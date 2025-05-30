2840a2964e478e9eca0bb3254b3e509e2191ab56
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:37:56 +0530

    Update HashMapExample.java

--- Module3/HashMapExample.java
+++ Module3/HashMapExample.java
@@ -4,35 +4,26 @@ import java.util.Scanner;
 
 public class HashMapExample {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         HashMap<Integer, String> studentMap = new HashMap<>();
-
-        // Adding student entries
         System.out.println("Enter student ID and name (type -1 to stop):");
         while (true) {
             System.out.print("Enter ID: ");
-            int id = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
-
-            if (id == -1) break; // Stop condition
-
+            int id = sc.nextInt();
+            sc.nextLine(); 
+            if (id == -1) break; 
             System.out.print("Enter name: ");
-            String name = scanner.nextLine();
-
+            String name = sc.nextLine();
             studentMap.put(id, name);
         }
-
-        // Retrieving student name by ID
         System.out.print("\nEnter an ID to search: ");
-        int searchId = scanner.nextInt();
-
+        int searchId = sc.nextInt();
         if (studentMap.containsKey(searchId)) {
             System.out.println("Student Name: " + studentMap.get(searchId));
         } else {
             System.out.println("ID not found!");
         }
-
-        scanner.close();
+        sc.close();
     }
 }
 
