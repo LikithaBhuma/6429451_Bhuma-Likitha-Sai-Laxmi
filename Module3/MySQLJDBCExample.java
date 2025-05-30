6cceae3d1ba138a5f3817a993d3ed9ca80ab3319
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:49:41 +0530

    Update MySQLJDBCExample.java

--- Module3/MySQLJDBCExample.java
+++ Module3/MySQLJDBCExample.java
@@ -3,22 +3,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 public class MySQLJDBCExample {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/your_database"; // Replace with actual DB name
-        String user = "root"; // Replace with your username
-        String password = "password"; // Replace with your password
-
+        String url = "jdbc:mysql://localhost:3306/database"; 
+        String user = "root"; 
+        String password = "password"; 
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM students");
-
             while (rs.next()) {
                 System.out.println("ID: " + rs.getInt("id") + ", Name: " + rs.getString("name") + ", Age: " + rs.getInt("age"));
             }
-
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
