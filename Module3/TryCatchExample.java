b2d71b914ae5df608d83b1e9dbd8cf700fba34bd
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 13:19:39 +0530

    Update TryCatchExample.java

--- Module3/TryCatchExample.java
+++ Module3/TryCatchExample.java
@@ -2,24 +2,17 @@ package Projects;
 import java.util.Scanner;
 public class TryCatchExample {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt user for input
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter first integer: ");
-        int num1 = scanner.nextInt();
-
+        int num1 = sc.nextInt();
         System.out.print("Enter second integer: ");
-        int num2 = scanner.nextInt();
-
+        int num2 = sc.nextInt();
         try {
-            // Attempt division
             int result = num1 / num2;
             System.out.println("Result: " + result);
         } catch (ArithmeticException e) {
-            // Handle division by zero
             System.out.println("Error: Division by zero is not allowed!");
         }
-
-        scanner.close();
+        sc.close();
     }
 }
