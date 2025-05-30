79e28057cd5668bc5b1d6fd4f92ea1395bff3169
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 13:12:44 +0530

    Update StudentList.java

--- Module3/StudentList.java
+++ Module3/StudentList.java
@@ -1,29 +1,23 @@
 package Projects;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class StudentList {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         ArrayList<String> students = new ArrayList<>();
-
         System.out.println("Enter student names (type 'done' to stop):");
-
         while (true) {
             System.out.print("Enter name: ");
-            String name = scanner.nextLine();
+            String name = sc.nextLine();
             if (name.equalsIgnoreCase("done")) {
                 break;
             }
             students.add(name);
         }
-
-        // Display all student names
         System.out.println("\nList of students:");
         for (String student : students) {
             System.out.println(student);
         }
-
-        scanner.close();
+        sc.close();
     }
 }
