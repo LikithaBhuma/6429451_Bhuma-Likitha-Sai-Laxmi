da756c9f97d6b00ac6a48da244f74e8d7ee8568d
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 13:10:04 +0530

    Update StringReversal.java

--- Module3/StringReversal.java
+++ Module3/StringReversal.java
@@ -2,25 +2,16 @@ package Projects;
 import java.util.Scanner;
 public class StringReversal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt user for input
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        // Method 1: Using a loop to reverse manually
-        String reversedLoop = "";
+        String input = sc.nextLine();
+        String r = "";
         for (int i = input.length() - 1; i >= 0; i--) {
-            reversedLoop += input.charAt(i);
+            r += input.charAt(i);
         }
-
-        // Method 2: Using StringBuilder's reverse() method
-        String reversedBuilder = new StringBuilder(input).reverse().toString();
-
-        // Display results
-        System.out.println("Reversed string (loop method): " + reversedLoop);
-        System.out.println("Reversed string (StringBuilder method): " + reversedBuilder);
-
-        scanner.close();
+        String reversed = new StringBuilder(input).reverse().toString();
+        System.out.println("Reversed string (loop method): " + r);
+        System.out.println("Reversed string (StringBuilder method): " + reversed);
+        sc.close();
     }
 }
