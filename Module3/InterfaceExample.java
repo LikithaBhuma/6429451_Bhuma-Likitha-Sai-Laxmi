1a2539f1bcc2bbdcdc215f0aa7afaa9e810e5540
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:46:34 +0530

    Update InterfaceExample.java

--- Module3/InterfaceExample.java
+++ Module3/InterfaceExample.java
@@ -1,32 +1,23 @@
 package Projects;
-//Define the Playable interface
 interface Playable {
-	void play(); // Abstract method to be implemented by classes
+	void play();
 }
-
-//Implement the interface in Guitar class
 class Guitar implements Playable {
 	@Override
 	public void play() {
-		System.out.println("Strumming the guitar 🎸");
+		System.out.println("Strumming the guitar ");
 	}
 }
-
-//Implement the interface in Piano class
 class Piano implements Playable {
 	@Override
 	public void play() {
-		System.out.println("Playing the piano 🎹");
+		System.out.println("Playing the piano ");
 	}
 }
-
 public class InterfaceExample {
 	public static void main(String[] args) {
-     // Instantiate objects
 		Playable guitar = new Guitar();
 		Playable piano = new Piano();
-
-     // Call play method
 		guitar.play();
 		piano.play();
 	}
