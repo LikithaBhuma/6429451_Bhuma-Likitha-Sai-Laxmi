d14ed0a6fc523559c736e2f2c4921a44dcfdadc4
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 13:24:30 +0530

    Update ArraySumAndAverage.java

--- Module3/ArraySumAndAverage.java
+++ Module3/ArraySumAndAverage.java
@@ -9,13 +9,12 @@ public class ArraySumAndAverage {
             System.out.println("Error: Please enter a positive number of elements.");
             return;
         }
-
         int[] numbers = new int[n]; 
         int sum = 0;
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
             numbers[i] = sc.nextInt();
-            sum += numbers[i]; // Accumulate sum
+            sum += numbers[i];
         }
         double average = (double) sum / n;
         System.out.println("Sum of elements: " + sum);
