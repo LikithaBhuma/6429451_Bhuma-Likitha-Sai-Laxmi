71967ba584b1434af16505912bbd24c1bf1970c0
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:55:57 +0530

    Update OperatorPrecedence.java

--- Module3/OperatorPrecedence.java
+++ Module3/OperatorPrecedence.java
@@ -1,16 +1,13 @@
 package Projects;
 public class OperatorPrecedence {
     public static void main(String[] args) {
-        // Example expressions with multiple operators
-        int result1 = 10 + 5 * 2;     // Multiplication (*) has higher precedence than addition (+)
-        int result2 = (10 + 5) * 2;   // Parentheses override precedence rules
-        int result3 = 20 / 4 + 3 * 2; // Division (/) and multiplication (*) before addition (+)
-        int result4 = 10 % 3 + 5 * 2; // Modulus (%) first, then multiplication (*), then addition (+)
-
-        // Display results
-        System.out.println("Result of 10 + 5 * 2 = " + result1);
-        System.out.println("Result of (10 + 5) * 2 = " + result2);
-        System.out.println("Result of 20 / 4 + 3 * 2 = " + result3);
-        System.out.println("Result of 10 % 3 + 5 * 2 = " + result4);
+        int res1 = 10 + 5 * 2;     // Multiplication has higher precedence than addition 
+        int res2 = (10 + 5) * 2;   // Parentheses override precedence rules
+        int res3 = 20 / 4 + 3 * 2; // Division  and multiplication before addition
+        int res4 = 10 % 3 + 5 * 2; // Modulus first, then multiplication, then addition
+        System.out.println("Result of 10 + 5 * 2 = " + res1);
+        System.out.println("Result of (10 + 5) * 2 = " + res2);
+        System.out.println("Result of 20 / 4 + 3 * 2 = " + res3);
+        System.out.println("Result of 10 % 3 + 5 * 2 = " + res4);
     }
 }
