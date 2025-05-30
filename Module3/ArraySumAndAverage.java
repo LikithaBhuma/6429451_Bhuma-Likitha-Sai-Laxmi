34276c0a05724273e57a35fa239f4e03f928d97d
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:26:03 +0530

    Update ArraySumAndAverage.java

--- Module3/ArraySumAndAverage.java
+++ Module3/ArraySumAndAverage.java
@@ -2,35 +2,24 @@ package Projects;
 import java.util.Scanner;
 public class ArraySumAndAverage {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt user for number of elements
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
-        int n = scanner.nextInt();
-
-        // Validate input
+        int n = sc.nextInt();
         if (n <= 0) {
             System.out.println("Error: Please enter a positive number of elements.");
             return;
         }
 
-        int[] numbers = new int[n]; // Array declaration
+        int[] numbers = new int[n]; 
         int sum = 0;
-
-        // Read elements into array
         System.out.println("Enter " + n + " numbers:");
         for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
+            numbers[i] = sc.nextInt();
             sum += numbers[i]; // Accumulate sum
         }
-
-        // Calculate average
         double average = (double) sum / n;
-
-        // Display results
         System.out.println("Sum of elements: " + sum);
         System.out.println("Average of elements: " + average);
-
         scanner.close();
     }
 }
