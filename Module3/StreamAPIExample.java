1a054b794d71128b5ca7d7aa486453007abb1423
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 13:08:01 +0530

    Update StreamAPIExample.java

--- Module3/StreamAPIExample.java
+++ Module3/StreamAPIExample.java
@@ -2,18 +2,10 @@ package Projects;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 public class StreamAPIExample {
     public static void main(String[] args) {
-        // Create a List of integers
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-        // Use Stream API to filter even numbers and collect results
-        List<Integer> evenNumbers = numbers.stream()
-                                           .filter(n -> n % 2 == 0)
-                                           .collect(Collectors.toList());
-
-        // Display the filtered list
+        List<Integer> evenNumbers = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
         System.out.println("Even numbers: " + evenNumbers);
     }
 }
