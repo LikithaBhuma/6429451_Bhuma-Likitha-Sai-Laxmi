78245d041c03d8bb6d5f5718407a66bed44cb263
LikithaBhuma <likitha.bhuma05@gmail.com>
2025-05-30 12:43:34 +0530

    Update InheritanceExample.java

--- Module3/InheritanceExample.java
+++ Module3/InheritanceExample.java
@@ -1,28 +1,20 @@
 package Projects;
-//Base class
 class Animal {
 	public void makeSound() {
 		System.out.println("Some generic animal sound...");
 	}
 }
-
-//Subclass inheriting from Animal
 class Dog extends Animal {
 	@Override
 	public void makeSound() {
 		System.out.println("Bark");
 	}
 }
-
 public class InheritanceExample {
 	public static void main(String[] args) {
-     // Instantiate base class
 		Animal genericAnimal = new Animal();
-     // Instantiate subclass
 		Dog myDog = new Dog();
-
-     // Call methods
-		genericAnimal.makeSound();  // Calls Animal's makeSound()
-		myDog.makeSound();          // Calls Dog's overridden makeSound()
+		genericAnimal.makeSound();  
+		myDog.makeSound();          
 	}
 }
